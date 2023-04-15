4dd45f8566a9e1b2084b73bea947cd6b370fb585
Robert Solomon <284481C@curtin.edu.au>
2023-04-16 07:39:21 +0800

    modified run code

--- Orepass/orepas_run.R
+++ Orepass/orepas_run.R
@@ -16,7 +16,7 @@ reps = 2
 sens_step=0
 run_id <<- makevalidname(as.character(Sys.time()))
 for(bogger in 5:6) {
-  for (n_trucks in 5:7) {
+  for (n_trucks in 5:6) {
     for (stope_access_const in 1:2) {
       sens_step=sens_step+1
       run_model(
